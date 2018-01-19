a53edf0bed528535d18345d419746508a7dc0f67
cracr <cracroule@gmail.com>
2018-01-19 15:28:33 +0100

    add a try catch on the sigmoid fct to handle float casting (issue with type of same data)

--- deeplearning/activation_fct.py
+++ deeplearning/activation_fct.py
@@ -2,7 +2,11 @@ import numpy as np
 
 
 def sigmoid(z):
-    return 1. / (1. + np.exp(-z))
+    try:
+        return 1. / (1. + np.exp(-z))
+    except AttributeError:
+        z1 = np.float64(z)
+        return 1. / (1. + np.exp(-z1))
 
 
 def d_sigmoid(z):
@@ -34,7 +38,7 @@ def d_tanh(z):
     return 1. - np.power(tanh(z), 2)
 
 
-def tanh_bacward(dA, z):
+def tanh_backward(dA, z):
     return np.multiply(dA, d_tanh(z))
 
 
